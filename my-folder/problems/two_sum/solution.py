73f8a28cf6f4fdf36d7b9f6d534e5dfd685e881a
Augustin Tsang <102135154+augustintsang@users.noreply.github.com>
2024-10-27 19:40:02 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] + nums[j] == target and i != j:
-                    return [i, j]
+            diff = target - nums[i]
+            if diff in dict and i != dict[diff]:
+                return[dict[diff], i]
+            dict[nums[i]] = i
